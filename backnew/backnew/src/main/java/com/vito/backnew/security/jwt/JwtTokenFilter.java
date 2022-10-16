4ba017c89c73384479401ab4715387d1e0406aa3
VictoriadeAntueno <victoriadeantueno@gmail.com>
2022-10-16 14:49:47 -0300

    security parte1

---
+++ backnew/backnew/src/main/java/com/vito/backnew/security/jwt/JwtTokenFilter.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.vito.backnew.security.jwt;
+
+import com.vito.backnew.security.service.UserDetailsImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+/**
+ *
+ * @author Usuario
+ */
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private final static Logger logger = LoggerFactory.getLogger(JwtProvider.class);
+
+    @Autowired
+    JwtProvider jwtProvider;
+    @Autowired
+    UserDetailsImpl userDetailsServiceImpl;
+}
---
+++ backnew/backnew/src/main/java/com/vito/backnew/security/repository/iRolRepository.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.vito.backnew.security.repository;
+
+import com.vito.backnew.security.Enums.RolNombre;
+import com.vito.backnew.security.entity.Rol;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Usuario
+ */
+@Repository
+public interface iRolRepository extends JpaRepository<Rol, Integer> {
+    Optional<Rol> findByRolNombre(RolNombre rolNombre);
+}
