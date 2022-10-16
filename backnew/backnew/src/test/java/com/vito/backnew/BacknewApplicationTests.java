aec8165104b0d4a76e7e742986ca6b0e685eaa0c
VictoriadeAntueno <victoriadeantueno@gmail.com>
2022-10-16 12:28:44 -0300

    conexion con database

---
+++ backnew/backnew/src/test/java/com/vito/backnew/BacknewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vito.backnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacknewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
