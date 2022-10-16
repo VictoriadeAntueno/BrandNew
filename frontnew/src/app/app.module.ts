bb7be61a426441797d6cfff40dad1e74a14aa7ce
VictoriadeAntueno <victoriadeantueno@gmail.com>
2022-10-16 11:57:02 -0300

    empiezo front

--- frontnew/src/app/app.module.ts
+++ frontnew/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule} from '@angular/common/http';
+import { ReactiveFormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    HeaderComponent,
+    BannerComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
